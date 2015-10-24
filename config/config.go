ca9915702dfb676a5d62a0c5c0b923d000d96e90
Rafael Leite <r.cmgleite@gmail.com>
2015-10-24 13:16:07 -0200

    fix. now config receive the config struct as parameter for Load

--- config/config.go
+++ config/config.go
@@ -6,21 +6,12 @@ import (
 	"gopkg.in/gcfg.v1"
 )
 
-// Config struct
-type Config struct {
-	Server struct {
-		Addr string
-		Port string
-	}
-}
-
 // Load a config from file and return a Config object
-func Load(filepath string) (*Config, error) {
-	cfg := Config{}
-	err := gcfg.ReadFileInto(&cfg, filepath)
+func Load(filepath string, cfg interface{}) error {
+	err := gcfg.ReadFileInto(cfg, filepath)
 	if err != nil {
 		fmt.Println("Error while loading config \n", err)
-		return nil, err
+		return err
 	}
-	return &cfg, nil
+	return nil
 }
